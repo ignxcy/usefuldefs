3253330baca0574b24c6384368e172e0ebccc645
ignxcy <ignacyy@proton.me>
2023-05-17 17:50:53 +0200

    Added the write function

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ def read(file):
             return f.read().strip()
     else:
         return "Usefuldefs: File {} does not exists.".format(file)
+# Write to a file
+def write(file, value):
+    if os.path.exists(file):
+        with open(file, "w") as f:
+            f.write(value)
+    else:
+        print("Usefuldefs: File {} does not exist".format(file))
