be83822e50eb80fe6321765212bf8fa34e3a177f
ignxcy <ignacyy@proton.me>
2023-05-17 17:45:50 +0200

    Added the read function

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import os
 # Do a system command
 def cmd(command):
     os.system(command)
+def read(file):
+    if os.path.exists(file):
+        with open(file, "r") as f:
+            return f.read().strip()
+    else:
+        print("Usefuldefs: File {} does not exists.".format(file))
